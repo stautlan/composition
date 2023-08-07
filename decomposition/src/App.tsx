760e9fea286559accfc20a27a94e2badcb6f2ddb
m.kurochkin <stautlan@yandex.ru>
2023-08-07 23:08:25 +0300

    Modified 2

--- decomposition/src/App.tsx
+++ decomposition/src/App.tsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
 import './App.css'
+import FindElement from './components/FindElement'
+import HeadItem from './components/HeadItem'
+import ImageElement from './components/ImageElement'
+import Menu from './components/Menu'
+import ThreeColumnElement from './components/ThreeColumnElement'
+
+const contextMenu = [
+  ["Сейчас", "Потом", "Рекомендуем"],
+  ["Видео", "Картинки", "Новости", "Карты", "Макет", "Переводчик", "Эфир"]
+];
+
+const context = [
+  "Функциональность и стилизацию реализовывать не нужно",
+  "чтобы видно было все блоки",
+  "Разбейте весь интерфейс"
+]
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Menu items={contextMenu[0]} />
+      <br />
+      <div>
+        {context.map((item) => <HeadItem text={item} />)}
+      </div>
+      <Menu items={contextMenu[1]} />
+      <FindElement />
+      <ImageElement>
+        <ThreeColumnElement />
+      </ImageElement>
     </>
   )
 }
